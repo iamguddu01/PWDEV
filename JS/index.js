b4e4f710afaa46e7c65c14070ddb4b93214d9e8f
Govind Kumar <govind.byte@gmail.com>
2025-10-10 17:45:55 +0530

    some chinks

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+// console.log('Hello class');
+// let mp = new Map([
+//     ["name", "Rahul"],
+//     ["name2", "govind"]
+// ]);
+// typeof mp
+// mp.get("name")
+// mp.set("name","guddu");
+// mp.get("name")
+
+console.log("line 1 :", b);
+let a = 10;
+var b = 20;
+console.log("line 3 : ", a , b);
---
+++ JS/logical-operator.js
@@ -0,0 +1,10 @@
+let marks = 70;
+if(marks < 33){
+    console.log("failed");
+}else if (marks >= 51 && marks <= 70){
+    console.log("b grade")
+}else if (marks >= 71 && marks <= 90){
+    console.log("a grade");
+}else{
+    console.log("A+ grade");
+}
