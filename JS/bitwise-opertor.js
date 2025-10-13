6f8f4927f16eec101d9d263d775940f9957592ab
Govind Kumar <govind.byte@gmail.com>
2025-10-13 17:52:29 +0530

    operator

--- JS/bitwise-opertor.js
+++ JS/bitwise-opertor.js
@@ -11,3 +11,7 @@ console.log(10 | 8);  // If any one of the bit is 1 than ans will 1 else 0
 console.log(9 ^ 8); // If any one bit is 1 than ans is 1,
                     //  If both the bits are one than ans 0,
                     //  If both bits are 0 ans is 0.
+
+
+// Bitwise NOT
+console.log(~5);   // Turns 1 to 0, 0 to 1. 
