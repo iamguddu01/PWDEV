03f09ab7f0e1e1323f4d65a30f4a974c926f5d6c
Govind Kumar <govind.byte@gmail.com>
2025-11-25 18:48:41 +0530

    25-nov

--- OOPS/oops.js
+++ OOPS/oops.js
@@ -42,20 +42,62 @@
 class Account {
     #pin = null; // Making properties private using prefix #
     #balance = null; 
+    #noOfWithdrawl = 0;
     constructor(holderName){
         this.holderName = holderName;
         this.#balance = 100;
         this.#pin = `${holderName}@1234`;
+        this.#noOfWithdrawl = 0;
     }
-    getBalance (){ // Getting private property usign getter methods
+    getBalance (pin){   // Getting private property usign getter methods
+        if(!pin || (pin !== this.#pin))
+            return {
+                status : false,
+                message : "Invalid Details"
+            }
         return this.#balance;
     }
-    setBalance(){
-        return this.#balance + 100;
+    deposit(amount, pin){
+        if(!pin || (pin !== this.#pin))
+            return {
+                status : false,
+                message : "Invalid Details"
+            }
+        this.#balance += amount;
+        return true;
+    }
+    withdraw(amount, pin){
+        if(!pin || (pin !== this.#pin))
+            return {
+                status : false,
+                message : "Invalid Details"
+            }
+        if(this.#noOfWithdrawl >= 3) // Checker to check the no of withdrawls if exceeded returns status of decline
+            return{
+                status : false,
+                message : "Limit reached of withdrawl"
+            }
+
+        if(amount > this.#balance){
+          console.log("Insufficient balance"); 
+        }else{
+         this.#noOfWithdrawl++;
+         this.#balance -= amount;
+            return true;
+        }
+        
     }
 }
 const a1 = new Account("Govind");
 console.log(a1.holderName + " : " + a1.balance + " : " + a1.p);
-console.log(a1.getBalance()); // Getter called
-console.log(a1.setBalance());
-console.log(a1.setBalance());
+console.log(a1.getBalance("Govind@1234")); // Getter called
+a1.deposit(10000, "Govind@1234");
+console.log(a1.getBalance("Govind@1234"));
+a1.withdraw(3000, "Govind@1234");
+a1.withdraw(3000, "Govind@1234");
+a1.withdraw(300, "Govind@1234");
+a1.withdraw(300, "Govind@1234");
+a1.withdraw(300, "Govind@1234");
+a1.withdraw(300, "Govind@1234");
+console.log(a1.withdraw(300, "Govind@1234"));
+console.log(a1.getBalance("Govind@1234"));
